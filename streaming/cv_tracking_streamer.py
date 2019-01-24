started fixing rotation bug (still buggy)

streaming/cv_tracking_streamer.py
@@ -60,11 +60,15 @@ def run_sender(ip, port, pattern_paths, video_path, pattern_match_scale=0.5, vid
     tracker.load_patterns(pattern_paths, pattern_ids, pattern_match_scale)
 
     # setup video capture
+    '''
     cap = cv.VideoCapture(
         "filesrc location=\"" + video_path + "\" ! decodebin ! videoconvert ! "
         "videoscale ! video/x-raw, width=" + str(video_width) +
         ", pixel-aspect-ratio=1/1 ! appsink"
     )
+    '''
+    cap = cv.VideoCapture("v4l2src ! videoscale ! video/x-raw, width=" + str(video_width) +
+                          ", pixel-aspect-ratio=1/1 ! appsink")
 
     if not cap.isOpened():
         print("Cannot capture test src. Exiting.")
@@ -72,7 +76,6 @@ def run_sender(ip, port, pattern_paths, video_path, pattern_match_scale=0.5, vid
 
     since_print = 0.0
     fps_collection = []
-    track_num = -1
     while True:
         start_time = time.time()
 
@@ -84,7 +87,6 @@ def run_sender(ip, port, pattern_paths, video_path, pattern_match_scale=0.5, vid
         # patterns to send
         upd_patterns = []
 
-        i = 0
         for res in tracker.track(frame):
             # update patterns to send
             osc_patterns[res.pattern_id].set_bnd(res.bnd)
@@ -94,19 +96,10 @@ def run_sender(ip, port, pattern_paths, video_path, pattern_match_scale=0.5, vid
             bnd_s = res.bnd.scaled(h, w)
             box = cv.boxPoints((
                 (bnd_s.x_pos, bnd_s.y_pos),
-                (bnd_s.width, bnd_s.height),
+                (-bnd_s.width, bnd_s.height),
                 bnd_s.angle
             ))
             frame = cv.polylines(frame, [np.int32(box)], True, 255, 3, cv.LINE_AA)
-            # draw frame in separate window
-            if i == track_num:
-                img = tracker.patterns[res.pattern_id].get_image().copy()
-                img_h, img_w = img.shape
-                pts = np.float32([[0, 0], [0, img_h - 1], [img_w - 1, img_h - 1], [img_w - 1, 0]]).reshape(-1, 1, 2)
-                M = cv.getPerspectiveTransform(pts, order_points(box))
-                img = cv.warpPerspective(img, M, (w, h))
-                cv.imshow("tracked-pattern", img)
-            i += 1
 
         # send patterns
         sender.send_patterns(upd_patterns)
@@ -117,15 +110,13 @@ def run_sender(ip, port, pattern_paths, video_path, pattern_match_scale=0.5, vid
         fps_collection.append(1.0 / elapsed_time)
         since_print += elapsed_time
         if since_print > 1.0:
-            print("avg fps:", int(sum(fps_collection) / float(len(fps_collection))))
+            #print("avg fps:", int(sum(fps_collection) / float(len(fps_collection))))
             fps_collection = []
             since_print = 0.0
 
         key_pressed = cv.waitKey(1) & 0xFF
         if key_pressed == ord('q'):
             break
-        elif key_pressed == ord('w'):
-            track_num = (track_num + 1) % len(tracker.patterns)
 
 
 def run_receiver(ip, port, w, h):
@@ -157,8 +148,8 @@ def run_receiver(ip, port, w, h):
                 bnd_s = p.get_bnd().scaled(h, w)
                 box = cv.boxPoints((
                     (bnd_s.x_pos, bnd_s.y_pos),
-                    (bnd_s.width, bnd_s.height),
-                    bnd_s.angle
+                    (-bnd_s.width, bnd_s.height),
+                    bnd_s.angle#bnd_s.angle if bnd_s.width < bnd_s.height else bnd_s.angle - 90
                 ))
                 frame = cv.polylines(frame, [np.int32(box)], True, 255, 3, cv.LINE_AA)
                 # draw transformed pattern
@@ -167,7 +158,7 @@ def run_receiver(ip, port, w, h):
                     img = images[uuid].copy()
                     img_h, img_w, img_c = img.shape
                     pts = np.float32([[0, 0], [0, img_h - 1], [img_w - 1, img_h - 1], [img_w - 1, 0]]).reshape(-1, 1, 2)
-                    M = cv.getPerspectiveTransform(pts, order_points(box))
+                    M = cv.getPerspectiveTransform(pts, box)#order_points(box))
                     frame = cv.warpPerspective(img, M, (w,h), frame, borderMode=cv.BORDER_TRANSPARENT)
 
         cv.imshow("FRAME", frame)

tracking/gst_cv_tracking.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import time
+import math
 
 from scipy.spatial import distance as dist
 from tracking.template_matching import SiftPattern, FlannMatcher
@@ -44,6 +45,28 @@ class GstCvTracking(object):
         self.patterns = {}
         self._frame_pattern = SiftPattern("THE-FRAME")
 
+    def vec_length(self, v):
+        return math.sqrt(sum([v[i]*v[i] for i in range(0,len(v))]))
+
+    def get_rot(self, M):
+        rad = -math.atan2(M[0][1], M[0][0])
+        deg = math.degrees(rad)
+        print("========")
+        print(rad)
+        print(deg)
+        return deg
+
+    def get_trans(self, M):
+        return [M[0][2], M[1][2]]
+
+    def get_scale(self, M):
+        s_x = np.sign(M[0][0]) * self.vec_length([M[0][0], M[0][1]])
+        s_y = np.sign(M[1][1]) * self.vec_length([M[1][0], M[1][1]])
+        return [s_x, s_y]
+
+    def decompose_mat(self, M):
+        return {"T": self.get_trans(M), "R": self.get_rot(M), "S": self.get_scale(M)}
+
     def track(self, image):
         """ SIFT/FLANN based object recognition is performed on image.
             frame should be a cv image. Matched patterns can be managed
@@ -60,9 +83,14 @@ class GstCvTracking(object):
                 pts = pattern.get_shape_points()
                 dst = cv.perspectiveTransform(pts, M)
                 rect = cv.minAreaRect(dst)
+                # rotation angle of area rect
+                # doesn't take into account the pattern orientation
+                angle = self.get_rot(M) + 90
+                if rect[1][0] < rect[1][1]:
+                    angle = 90 + angle
                 bnd = OscPatternBnd(
                     rect[0][0], rect[0][1],  # pos
-                    rect[2],                 # angle
+                    angle,                   # rotation
                     rect[1][0], rect[1][1]   # size
                 ).normalized(h, w)
                 res.append(CvTrackingResult(pattern.get_id(), bnd))